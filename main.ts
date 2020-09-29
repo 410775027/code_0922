d1883051f74740ea2a4d579fa6acaeff61a7f126
410775027 <71691336+410775027@users.noreply.github.com>
2020-09-29 16:20:29 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 basic.forever(function () {
-    basic.showNumber(0)
-    basic.pause(1000)
-    basic.showNumber(1)
-    basic.pause(1000)
-    basic.showNumber(2)
-    basic.pause(1000)
-    basic.showNumber(3)
-    basic.pause(1000)
-    basic.showNumber(4)
+    for (let index = 0; index <= 9; index++) {
+        basic.showNumber(index)
+        basic.pause(200)
+    }
+    for (let index = 0; index <= 9; index++) {
+        basic.showNumber(9 - index)
+        basic.pause(200)
+    }
 })
